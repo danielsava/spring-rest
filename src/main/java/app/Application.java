38b3e737706d7ddd24eb7e912032cfb22e703062
Daniel Sava <danielsavas@gmail.com>
2020-04-23 22:40:32 -0300

    Refactoring

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -1,13 +1,13 @@
-package br.com.savasoft.examples.springrest;
+package app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringRestApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringRestApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
