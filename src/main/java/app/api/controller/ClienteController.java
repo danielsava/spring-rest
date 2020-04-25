f1ada83333ac539377ad59a173a63a68b2b23fb1
Daniel Sava <danielsavas@gmail.com>
2020-04-24 22:24:22 -0300

    Aula 2

---
+++ src/main/java/app/api/controller/ClienteController.java
@@ -0,0 +1,62 @@
+package app.api.controller;
+
+
+import app.domain.model.Cliente;
+import app.domain.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/clientes")
+public class ClienteController {
+
+    //@PersistenceContext
+    //private EntityManager manager;
+
+    @Autowired
+    ClienteRepository clienteRepository;
+
+    @GetMapping
+    public List<Cliente> listar() {
+        return clienteRepository.findAll();
+        //return manager.createQuery("from Cliente", Cliente.class).getResultList();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Cliente> buscar(@PathVariable Long id) {
+        Optional<Cliente> c = clienteRepository.findById(id);
+        return c.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.noContent().build());
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente adicionar(@RequestBody Cliente cliente) {
+        return clienteRepository.save(cliente);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Cliente> atualizar(@PathVariable Long id, @RequestBody Cliente cliente) {
+
+        if(!clienteRepository.existsById(id))
+            return ResponseEntity.noContent().build();
+
+        cliente.setId(id);
+        cliente = clienteRepository.save(cliente);
+        return ResponseEntity.ok().body(cliente);
+
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> remover(@PathVariable Long id) {
+        if(!clienteRepository.existsById(id))
+            return ResponseEntity.noContent().build();
+        clienteRepository.deleteById(id);
+        return ResponseEntity.ok().build();
+    }
+
+}
--- src/main/java/app/domain/model/Cliente.java
+++ src/main/java/app/domain/model/Cliente.java
@@ -1,11 +1,39 @@
-package app.model;
+package app.domain.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
 public class Cliente {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String nome;
+
     private String email;
+
     private String telefone;
+
+
+    /* Equals e HashCode a partir do ID */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cliente cliente = (Cliente) o;
+        return Objects.equals(id, cliente.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
 
     public Long getId() {
         return id;
